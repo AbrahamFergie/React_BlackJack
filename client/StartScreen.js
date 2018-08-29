f50bcda082a6bc1df38b33ce62a12a409e02501e
abrahamfergie <jhnfgie1989@gmail.com>
2018-08-28 17:43:27 -0700

    configuring deployment

--- client/StartScreen.js
+++ client/StartScreen.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react'
 import Board from './Board'
 
 export default class StartScreen extends Component {
-  constructor() {
+  constructor(props) {
+    super(props)
     this.state = {
       visibility: true,
       value: ''
